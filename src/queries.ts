daec7e4005d8f423bccf6c25d51b3273d9c80eec
tpho216 <tpho216@gmail.com>
2021-06-20 16:32:31 +1000

    changes

--- src/queries.ts
+++ src/queries.ts
@@ -1,19 +1,28 @@
 const Pool = require('pg').Pool;
-
-const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host:  process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PW,
-    port: 5432,
+const {Client} = require('pg');
+const client = new Client( {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+client.connect(); //SSL connection to database accepted by Heroku postgreSQL add on
+
+// const pool = new Pool({
+//     user: process.env.DATABASE_USER,
+//     host:  process.env.DATABASE_HOST,
+//     database: process.env.DATABASE_NAME,
+//     password: process.env.DATABASE_PW,
+//     port: 5432,
+// });
 
 
 const getModelAnswers = (
     request: { params: { id: string } },
     response: { status: (arg0: number) => { (): any; new (): any; json: { (arg0: any): void; new (): any } } },
 ) => {
-    pool.query("SELECT * FROM modelanswer ORDER BY id ASC", (error: any, results: { rows: any; }) => {
+    client.query("SELECT * FROM modelanswer ORDER BY id ASC", (error: any, results: { rows: any; }) => {
         if (error) {
             console.log("ERROR: " + error);
             throw error;
@@ -26,7 +35,7 @@ const getQuestions = (
     request: { params: { id: string } },
     response: { status: (arg0: number) => { (): any; new (): any; json: { (arg0: any): void; new (): any } } },
 ) => {
-    pool.query("SELECT * FROM quizquestion ORDER BY id ASC", (error: any, results: { rows: any; }) => {
+    client.query("SELECT * FROM quizquestion ORDER BY id ASC", (error: any, results: { rows: any; }) => {
         if (error) {
             console.log("ERROR: " + error);
             throw error;
